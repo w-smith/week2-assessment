0a05300b22a7325e94cef5ba3be9fbc8f645d36f
w-smith <wsmith@bex.net>
2017-12-07 19:54:03 -0700

    got racers moving indepedently

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ console.log("It's me, ya boii... Javascript");
 // player 1 keypress function
 
 $(window).keypress(function(q) {
-    if(q.which == 113)
-    console.log("racer1");
-$("#racer1").animate({left: "+=50"});
+    if(q.keyCode == 113)
+    // console.log("racer1");
+$("#racer1").animate({marginLeft: "+=50px"},-150);
 
 });
 
@@ -16,9 +16,9 @@ $("#racer1").animate({left: "+=50"});
 
 
 $(window).keypress(function(p) {
-    if(p.which == 112)
-    console.log("racer2");
-$("#racer2").animate({left: "+=50"});
+    if(p.keyCode == 112)
+    // console.log("racer2");
+$("#racer2").animate({marginLeft: "+=50px"},-150);
 });
 
 
