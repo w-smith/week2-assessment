5de4664c99a2f85daed7395b30fbc1accdcabab1
w-smith <wsmith@bex.net>
2017-12-07 18:42:54 -0700

    getting racers to move

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 console.log("It's me, ya boii... Javascript");
 
 
-$( "#window" ).keypress(function(e) {
-    if(e.which == 13) {
-        console.log('You pressed enter!');
-}});
+// player 1 keypress function
 
-
-
-$( "#racer2" ).keyup(function(p) {
-  console.log( "Handler for .keypress() called." );
+$(window).keypress(function(q) {
+    if(q.which == 113)
+    console.log("racer1");
+ $("#racer1").animate({width: "+=100px"});
 });
+
+
+
+// player 2 keypress function
+
+
+$(window).keypress(function(p) {
+    if(p.which == 112)
+    console.log("racer2");
+$("#racer2").animate({width: "+=100px"});
+});
+
+
