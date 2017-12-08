8eb0d8c364f99e957b8f9afc207bfd3453c0befc
w-smith <wsmith@bex.net>
2017-12-07 18:16:46 -0700

    adding racers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("It's me, ya boii... Javascript");
+
+
+$( "#window" ).keypress(function(e) {
+    if(e.which == 13) {
+        console.log('You pressed enter!');
+}});
+
+
+
+$( "#racer2" ).keyup(function(p) {
+  console.log( "Handler for .keypress() called." );
+});
