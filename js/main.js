b104f6a5effa2602610ade363885e5b03ff20bf5
w-smith <wsmith@bex.net>
2017-12-07 20:59:13 -0700

    adding winner alert

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 console.log("It's me, ya boii... Javascript");
 
 
+var arr1 = 0;
+
+
+var arr2 = 0;
+
+$(document).keypress(function(q) {
+	if(q.keyCode == 113)
+     arr1 += 1; 
+	if (arr1 === 20) {alert("player 1 wins!"); }
+});
+
+$(document).keypress(function(p) {
+	if(p.keyCode == 112)
+     arr2 += 1; 
+ 	if (arr2 === 20) {alert("player 2 wins!"); }
+});
+
+
+
+
+
+
 // player 1 keypress function
 
 $(window).keypress(function(q) {
     if(q.keyCode == 113)
     // console.log("racer1");
-$("#racer1").animate({marginLeft: "+=50px"},-150);
+$("#racer1").animate({marginLeft: "+=4%"},-200);
 
 });
 
@@ -18,7 +40,10 @@ $("#racer1").animate({marginLeft: "+=50px"},-150);
 $(window).keypress(function(p) {
     if(p.keyCode == 112)
     // console.log("racer2");
-$("#racer2").animate({marginLeft: "+=50px"},-150);
+$("#racer2").animate({marginLeft: "+=4%"},-200);
 });
+
+
+
 
 
